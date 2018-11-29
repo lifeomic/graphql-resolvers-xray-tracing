0f2d5ff78a105ed8953176fa82976ffcefbcc79b
Zhenya Ragulin <zhenya.ragulin@lifeomic.com>
2018-11-29 15:09:30 -0500

    Revert "Disable xray logs in integration tests"
    This reverts commit 7e3d61bb831c398a16fac5882b8e7eaf8059c3dc.

--- src/traceResolvers.js
+++ src/traceResolvers.js
@@ -14,16 +14,14 @@ function fieldPathFromInfo (info) {
 }
 
 const tracer = function (resolver, parent, args, ctx, info) {
-  if (process.env.DISABLE_XRAY_LOGGING) {
-    return resolver();
-  }
   let result;
+
   const fieldPath = fieldPathFromInfo(info);
   AWSXRay.captureAsyncFunc(`GraphQL ${fieldPath}`, function (subsegment) {
     result = resolver();
-    // Note 1: When AWS_XRAY_CONTEXT_MISSING is set to LOG_MISSING and no context was
+
+    // When AWS_XRAY_CONTEXT_MISSING is set to LOG_MISSING and no context was
     // found, then the subsegment will be null and nothing should be done
-    // Note 2: We don't trace requests during integration tests since it makes logs unreadable
     if (subsegment) {
       if (isPromise(result)) {
         result.then(function () {
